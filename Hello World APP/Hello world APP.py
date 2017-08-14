d7d13f53c20b9f3ed147aa1c69fa7a55fb3f6fd8
Huang-JE Chen <mu0837@gmail.com>
2017-08-14 11:05:30 +0930

    my Hello World APP by Cloud

---
+++ Hello World APP/Hello world APP.py
@@ -0,0 +1,5 @@
+Python 3.6.0 (v3.6.0:41df79263a11, Dec 23 2016, 08:06:12) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print (" Hello World APP")
+ Hello World APP
+>>> 
